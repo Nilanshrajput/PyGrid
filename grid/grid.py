4c215999ec8518f6fec673b72534480e0480105f
Nilanshrajput <2017csb1092@iitrpr.ac.in>
2020-07-29 04:14:50 +0530

    rand

--- grid/grid.py
+++ grid/grid.py
@@ -7,7 +7,7 @@ import sys
 
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
-
+import syfertext
 from .app import create_app
 
 parser = argparse.ArgumentParser(description="Run Grid Gateway application.")
